a181f7ea4832a7950995090176c3856fb5c2da55
Indra Rudianto <indrarudianto.official@gmail.com>
2020-02-13 16:02:39 +0700

    Fix test ml bugs

--- tests/test_ml.py
+++ tests/test_ml.py
@@ -7,7 +7,7 @@ class SeismicEnergyTest(unittest.TestCase):
 
     def test_compute_seismic_energy(self):
         self.assertAlmostEqual(
-            10**(12.8), ml.compute_seismic_energy(2.0/3.0), delta=1e-3)
+            10**(12.8)/10**12, ml.compute_seismic_energy(2.0/3.0), delta=1e-3)
 
 
 if __name__ == '__main__':
